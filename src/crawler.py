baa1e0696bf73a466e2fcc659ff41266507c7b24
yinhoe <yinthehoe@gmail.com>
2026-05-06 00:32:36 +0100

    test(v0.1): Created tests for retrieve_links() in crawler.py

---
+++ src/crawler.py
@@ -0,0 +1,14 @@
+"""Functions for a web crawler."""
+
+def retrieve_page():
+    """Retrieves an HTML page via a GET request."""
+    return
+    
+def retrieve_links():
+    """Retrieves the unique url links in an HTML page."""
+    return
+
+def crawl():
+    """For now it retrieves a list of links accessible from the root url."""
+    return
+
---
+++ tests/test_crawler.py
@@ -0,0 +1,89 @@
+"""Tests for src/crawler.py"""
+
+import pytest
+from src.crawler import *
+
+
+@pytest.fixture
+def html_page(request):
+    """Modifiable fixture for an HTML page."""
+    page = """<html lang="en">
+              <head><meta charset="UTF-8"><title>Mock HTML Page</title>
+              <link rel="stylesheet" href="/static/bootstrap.min.css"></head>
+              <body><div class="container">"""
+
+    # Adds links to the body of the HTML page
+    for link in request.param:
+        page += f"<a href={link}>link</a></span>"
+    
+    page += "</div></body></html>"
+    return page
+
+
+class TestRetrieveLinks:
+    """Tests for retreive_links()"""
+    
+    @pytest.mark.parametrize(
+        "html_page",
+        [{"www.google.com", "https://minerva.leeds.ac.uk"}],
+        indirect=True
+    )
+    def test_absolute_urls(self, html_page):
+        """Finds absolute urls."""
+        links = retrieve_links(html_page)
+        assert links == {"www.google.com", "https://minerva.leeds.ac.uk"}
+
+    @pytest.mark.parametrize(
+        "html_page",
+        [{"/about", "/users/123"}],
+        indirect=True
+    )
+    def test_relative_urls(self, html_page):
+        """Finds relative urls."""
+        base_url = "https://quotes.toscrape.com"
+        links = retrieve_links(html_page)
+        assert links == {base_url+"/about", base_url+"/users/123"}
+
+    @pytest.mark.parametrize(
+        "html_page",
+        [{"www.google.com", "/about", "https://site.org", "/users/123"}],
+        indirect=True
+    )
+    def test_absolute_and_relative_urls(self, html_page):
+        """Finds multiple mixed urls."""
+        base_url = "https://quotes.toscrape.com"
+        links = retrieve_links(html_page)
+        assert links == {
+            "www.google.com", "https://site.org",
+            base_url+"/about", base_url+"/users/123"
+        }
+
+    @pytest.mark.parametrize("html_page", [{}], indirect=True)
+    def test_no_urls(self, html_page):
+        """Handles pages with no links."""
+        links = retrieve_links(html_page)
+        assert links == {}
+
+    @pytest.mark.parametrize(
+        "html_page",
+        [{"/users/1", "www.google.com", "/users/1"}],
+        indirect=True
+    )
+    def test_duplicate_urls(self, html_page):
+        """Parses duplicate urls correctly."""
+        base_url = "https://quotes.toscrape.com"
+        links = retrieve_links(html_page)
+        assert links == {"www.google.com", base_url+"/about"}
+
+    def test_disallowed(self, html_page):
+        """Avoids a disallowed link from a robot.txt file."""
+        assert False
+
+class TestRetrievePage:
+    def test_basic():
+        """Fetches and downloads a correct html page using a link."""
+        assert False
+
+    def test_error():
+        """Handles errors."""
+        assert False
